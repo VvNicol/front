3869d96ebd7cd463b3f26d6f8c3bd41ba4a1d6e1
D1ante <alentaborda@gmail.com>
2024-11-30 14:55:35 +0100

    organizacion de carpetas

---
+++ src/main/webapp/js/inicio/clubRegistro.js
@@ -0,0 +1,73 @@
+// registro.js
+
+export function imgFondoRegistroClub() {
+    const divContenedor = document.createElement('div');
+    divContenedor.classList.add('container-fluid', 'p-0');
+
+    const divCol = document.createElement('div');
+    divCol.className = 'col';
+
+    divContenedor.appendChild(divCol);
+
+    const img = imgFondo();
+    divCol.appendChild(img);
+
+    img.onload = function () {
+        img.classList.add('img-loaded');
+    };
+    return divContenedor;
+}
+
+export function formularioRegistroClub() {
+    const div = document.createElement('div');
+    div.classList.add('container', 'd-flex', 'justify-content-center', 'align-items-center', 'vh-100');
+
+    const formulario = `
+        <div class="card bg-dark text-light shadow-lg p-4" style="width: 600px; background-color: rgba(0, 0, 0, 0.7); border-radius: 10px;">
+            <div class="card-body">
+                <h2 class="card-title text-center mb-4" style="font-weight: bold; text-transform: uppercase; font-size: 1.5rem;">Registrarse</h2>
+                <form id="registrationForm">
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="fullname" class="form-label"><i class="fa-solid fa-user me-2"></i> Nombre del Club:</label>
+                            <input type="text" id="fullname" name="fullname" class="form-control bg-light text-dark" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="phone" class="form-label"><i class="fa-solid fa-id-card me-2"></i> Alias del Club:</label>
+                            <input type="tel" id="alias" name="alias" class="form-control bg-light text-dark" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="password" class="form-label"><i class="fa-solid fa-lock me-2"></i> Contraseña:</label>
+                            <input type="password" id="password" name="password" class="form-control bg-light text-dark" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="confirmPassword" class="form-label"><i class="fa-solid fa-lock me-2"></i> Confirmar Contraseña:</label>
+                            <input type="password" id="confirmPassword" name="confirmPassword" class="form-control bg-light text-dark" required>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn w-100 mt-3" style="background-color: #ffbd59; color: black; font-weight: bold;">Registrar</button>
+                </form>
+            </div>
+        </div>
+
+    `;
+
+    div.innerHTML = formulario;
+
+    return div;
+}
+
+
+function imgFondo() {
+    const img = document.createElement('img');
+    img.id = 'fondo';
+    img.src = "./img/harley-davidson.jpg";
+    img.alt = "Foto sobre un motor de moto en blanco y negro";
+    img.classList.add('img-fluid');
+
+    return img;
+}
--- src/main/webapp/js/inicio/registro.js
+++ src/main/webapp/js/inicio/registro.js
@@ -1,73 +1,69 @@
-// registro.js
+//altaUsuario.js
 
-export function imgFondoRegistroClub() {
-    const divContenedor = document.createElement('div');
-    divContenedor.classList.add('container-fluid', 'p-0');
+export function imgFondoRegistro() {
+	const divContenedor = document.createElement('div');
+	divContenedor.classList.add('container-fluid', 'p-0');
 
-    const divCol = document.createElement('div');
-    divCol.className = 'col';
+	const divCol = document.createElement('div');
+	divCol.className = 'col';
 
-    divContenedor.appendChild(divCol);
+	divContenedor.appendChild(divCol);
 
-    const img = imgFondo();
-    divCol.appendChild(img);
+	const img = imgFondo();
+	divCol.appendChild(img);
 
-    img.onload = function () {
-        img.classList.add('img-loaded');
-    };
-    return divContenedor;
+	img.onload = function() {
+		img.classList.add('img-loaded');
+	};
+	return divContenedor;
 }
 
-export function formularioRegistroClub() {
-    const div = document.createElement('div');
-    div.classList.add('container', 'd-flex', 'justify-content-center', 'align-items-center', 'vh-100');
+export function registro() {
+	const div = document.createElement('div');
+	div.classList.add('container', 'd-flex', 'justify-content-center', 'align-items-center', 'vh-100');
 
-    const formulario = `
-        <div class="card bg-dark text-light shadow-lg p-4" style="width: 600px; background-color: rgba(0, 0, 0, 0.7); border-radius: 10px;">
-            <div class="card-body">
-                <h2 class="card-title text-center mb-4" style="font-weight: bold; text-transform: uppercase; font-size: 1.5rem;">Registrarse</h2>
-                <form id="registrationForm">
-                    <div class="row mb-3">
-                        <div class="col">
-                            <label for="fullname" class="form-label"><i class="fa-solid fa-user me-2"></i> Nombre del Club:</label>
-                            <input type="text" id="fullname" name="fullname" class="form-control bg-light text-dark" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col">
-                            <label for="phone" class="form-label"><i class="fa-solid fa-id-card me-2"></i> Alias del Club:</label>
-                            <input type="tel" id="alias" name="alias" class="form-control bg-light text-dark" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-md-6">
-                            <label for="password" class="form-label"><i class="fa-solid fa-lock me-2"></i> Contraseña:</label>
-                            <input type="password" id="password" name="password" class="form-control bg-light text-dark" required>
-                        </div>
-                        <div class="col-md-6">
-                            <label for="confirmPassword" class="form-label"><i class="fa-solid fa-lock me-2"></i> Confirmar Contraseña:</label>
-                            <input type="password" id="confirmPassword" name="confirmPassword" class="form-control bg-light text-dark" required>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn w-100 mt-3" style="background-color: #ffbd59; color: black; font-weight: bold;">Registrar</button>
-                </form>
-            </div>
-        </div>
+	const formulario = `
+
+	<div class="d-flex flex-column flex-md-row justify-content-between align-items-md-center">
+		<div class="mb-2 mb-md-0">
+			<h5 class="mb-1">
+				Club: <span>${club.nombre}</span>
+			</h5>
+			<p class="mb-0">
+				Correo: <span>${club.correo}</span>
+			</p>
+			<p class="mb-0">
+				Descripción: <span class="text-truncate d-inline-block"
+					style="max-width: 300px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+					${club.descripcion} </span>
+				<button
+					class="btn-expandir-descripcion btn btn-link p-0 text-decoration-none">
+					Ver más</button>
+			</p>
+		</div>
+		<div class="d-flex flex-column flex-sm-row">
+			<button
+				class="btn-eliminar btn btn-danger btn-sm mb-2 mb-sm-0 me-0 me-sm-2"
+				data-correo="${club.correo}">Eliminar</button>
+			<button class="btn-editar btn btn-custom-warning btn-sm"
+				data-id="${club.id}">Editar</button>
+		</div>
+	</div>
 
     `;
 
-    div.innerHTML = formulario;
+	div.innerHTML = formulario;
 
-    return div;
+	return div;
 }
 
 
 function imgFondo() {
-    const img = document.createElement('img');
-    img.id = 'fondo';
-    img.src = "./img/harley-davidson.jpg";
-    img.alt = "Foto sobre un motor de moto en blanco y negro";
-    img.classList.add('img-fluid');
+	const img = document.createElement('img');
+	img.id = 'fondo';
+	img.src = "./img/harley-davidson.jpg";
+	img.alt = "Foto sobre un motor de moto en blanco y negro";
+	img.classList.add('img-fluid');
 
-    return img;
+	return img;
 }
